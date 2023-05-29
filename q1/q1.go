23d22c379512c4a7cbe86d129825ceace91eb101
bruno isaac sanches tavares <127205757+zexxx168@users.noreply.github.com>
2023-05-28 21:43:51 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,12 +1,8 @@
 package q1
 
-//Você está trabalhando em um projeto de gerenciamento de uma escola. O sistema precisa armazenar informações sobre os alunos, incluindo seu nome, idade e as matérias em que estão matriculados, juntamente com suas respectivas notas. Você decidiu usar structs e mapas para representar essas informações.
-//
-//No entanto, você descobriu que existem dois conjuntos de dados diferentes sobre os alunos. Cada conjunto de dados é representado por um mapa. O mapa "studentData1" contém informações sobre as notas dos alunos para a primeira metade do semestre, enquanto o mapa "studentData2" contém informações sobre as notas para a segunda metade do semestre.
-//
-//Sua tarefa é criar uma função chamada "mergeStudentData" que recebe os mapas "studentData1" e "studentData2" como parâmetros e retorna um novo mapa que contém as informações combinadas dos dois conjuntos de dados.
-//
-//O objetivo é combinar as informações de cada aluno, preservando o nome e a idade, e atualizando as matérias e notas de acordo com o mapa mais recente. Lembre-se de que um aluno pode estar matriculado em diferentes matérias em cada metade do semestre.
+import (
+	"fmt"
+)
 
 type Student struct {
 	Name     string
@@ -14,7 +10,68 @@ type Student struct {
 	Subjects map[string]float64
 }
 
-func MergeStudentData(studentData1 map[string]Student, studentData2 map[string]Student) map[string]Student {
-	// Seu código aqui
-	return nil
+func mergeStudentData(studentData1, studentData2 map[string]Student) map[string]Student {
+	result := make(map[string]Student)
+
+	// Adiciona os alunos do studentData1 ao resultado
+	for name, student := range studentData1 {
+		result[name] = student
+	}
+
+	// Atualiza as notas dos alunos com base no studentData2
+	for name, student := range studentData2 {
+		if _, ok := result[name]; ok {
+			// O aluno já existe no resultado, atualiza as matérias e notas
+			for subject, grade := range student.Subjects {
+				result[name].Subjects[subject] = grade
+			}
+		} else {
+			// O aluno não existe no resultado, adiciona-o
+			result[name] = student
+		}
+	}
+
+	return result
+}
+
+func main() {
+	studentData1 := map[string]Student{
+		"John": Student{
+			Name: "John",
+			Age:  20,
+			Subjects: map[string]float64{
+				"Math":    8.5,
+				"Science": 7.8,
+			},
+		},
+		"Alice": Student{
+			Name: "Alice",
+			Age:  22,
+			Subjects: map[string]float64{
+				"History": 9.2,
+			},
+		},
+	}
+
+	studentData2 := map[string]Student{
+		"John": Student{
+			Name: "John",
+			Age:  20,
+			Subjects: map[string]float64{
+				"Science": 8.0,
+				"English": 7.5,
+			},
+		},
+		"Bob": Student{
+			Name: "Bob",
+			Age:  21,
+			Subjects: map[string]float64{
+				"Math":    6.7,
+				"Physics": 8.9,
+			},
+		},
+	}
+
+	result := mergeStudentData(studentData1, studentData2)
+	fmt.Println(result)
 }
