ba3f0bc389f9557174b4298816eaa199ac7a9cf5
bruno isaac sanches tavares <127205757+zexxx168@users.noreply.github.com>
2023-05-28 21:23:08 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,17 +1,17 @@
 function mergeStudentData(studentData1, studentData2) {
   let mergedData = {};
 
-  // Itera sobre as chaves (nomes dos alunos) do mapa studentData1
+ 
   for (let name in studentData1) {
     if (name in studentData2) {
-      // Se o nome do aluno também está presente no mapa studentData2, combina as informações
+     
       mergedData[name] = {
         name: name,
         age: studentData1[name].age,
         subjects: { ...studentData1[name].subjects, ...studentData2[name].subjects }
       };
     } else {
-      // Se o nome do aluno não está presente no mapa studentData2, copia as informações do mapa studentData1
+ 
       mergedData[name] = {
         name: name,
         age: studentData1[name].age,
@@ -19,11 +19,8 @@ function mergeStudentData(studentData1, studentData2) {
       };
     }
   }
-
-  // Itera sobre as chaves (nomes dos alunos) do mapa studentData2
   for (let name in studentData2) {
-    if (!(name in studentData1)) {
-      // Se o nome do aluno não está presente no mapa studentData1, copia as informações do mapa studentData2
+    if (!(name in studentData1)) 
       mergedData[name] = {
         name: name,
         age: studentData2[name].age,
