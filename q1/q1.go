fcf0623fa33b56f03916257a9dc4c5d2fc624f54
bruno isaac sanches tavares <127205757+zexxx168@users.noreply.github.com>
2023-05-28 21:44:49 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -13,20 +13,17 @@ type Student struct {
 func mergeStudentData(studentData1, studentData2 map[string]Student) map[string]Student {
 	result := make(map[string]Student)
 
-	// Adiciona os alunos do studentData1 ao resultado
+	
 	for name, student := range studentData1 {
 		result[name] = student
 	}
-
-	// Atualiza as notas dos alunos com base no studentData2
 	for name, student := range studentData2 {
 		if _, ok := result[name]; ok {
-			// O aluno já existe no resultado, atualiza as matérias e notas
 			for subject, grade := range student.Subjects {
 				result[name].Subjects[subject] = grade
 			}
 		} else {
-			// O aluno não existe no resultado, adiciona-o
+			
 			result[name] = student
 		}
 	}
